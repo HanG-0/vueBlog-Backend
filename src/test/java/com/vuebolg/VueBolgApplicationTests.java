ae2912b5a8fa16683c479a6c14b76718d1a264d9
HanG <221250007@smail.nju.edu.cn>
2023-11-26 15:41:25 +0800

    12

---
+++ src/test/java/com/vuebolg/VueBolgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuebolg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBolgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
