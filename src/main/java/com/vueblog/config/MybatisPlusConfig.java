44e219eb5e0a2d57aa0490d76aafbddcf474c507
hang <221250007@smail.nju.edu.cn>
2024-01-18 13:29:36 +0800

    最开始的java后端项目框架，需要配置mysql才能使用项目中的数据库。先克隆到本地，在idea上创建新的git分支（如backend-chenWenHong）然后提交到github，

--- src/main/java/com/vueblog/config/MybatisPlusConfig.java
+++ src/main/java/com/vueblog/config/MybatisPlusConfig.java
@@ -1,28 +1,32 @@
 package com.vueblog.config;
 
-//import com.baomidou.mybatisplus.extension.plugins.;
+//新版3.5 mybatis_plus配置
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//"""
-//通过@mapperScan注解指定要变成实现类的接口所在的包
-// 然后包下面的所有接口在编译之后都会生成相应的实现类。
-// PaginationInterceptor是一个分页插件。
-//"""
-//@Configuration
-//@EnableTransactionManagement
-//@SpringBootApplication
-//指定要变成实现类的接口所在的包，然后包下面的所有接口在编译之后都会生成相应的实现类
-//添加位置：是在Springboot启动类上面添加
-//@MapperScan("com.vueblog.mapper")
-//public class MybatisPlusConfig {
-//    @Bean
-//    public PaginationInterceptor paginationInterceptor() {
-//        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+//      PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
 //        return paginationInterceptor;
 //    }
+/*
+通过@mapperScan注解指定要变成实现类的接口所在的包
+然后包下面的所有接口在编译之后都会生成相应的实现类。
+PaginationInterceptor是一个分页插件。
+ */
+    @Configuration
+    @EnableTransactionManagement
+    @SpringBootApplication
+//    指定要变成实现类的接口所在的包，然后包下面的所有接口在编译之后都会生成相应的实现类
+//    添加位置：是在Springboot启动类上面添加
+    @MapperScan("com.vueblog.mapper")
+    public class MybatisPlusConfig {
+        @Bean
+        public PaginationInnerInterceptor paginationInnerInterceptor() {
+            PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+            return paginationInnerInterceptor;
 
-//}
+        }
+    }
